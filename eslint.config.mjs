a5ada241a6d5544dd17742cc5b2fe5d9d8cea722
Hoxmot <hoxmot@gmail.com>
2025-01-01 17:54:29 +0100

    Hopefully configured linting & formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
 import path from "node:path";
 import { includeIgnoreFile } from "@eslint/compat";
 import { fileURLToPath } from "node:url";
@@ -10,10 +11,16 @@ const __dirname = path.dirname(__filename);
 const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
+export default tseslint.config(
   includeIgnoreFile(gitignorePath),
   { files: ["**/*.{ts}"] },
-  { languageOptions: { globals: globals.node } },
+  {
+    languageOptions: { globals: globals.node },
+    linterOptions: {
+      noInlineConfig: true,
+    },
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+  tseslint.configs.recommended,
+  prettierConfig,
+);
